99a170237c0b2f52fc297c8418036c2f046e8134
Cristian <cr.mansilla@duocuc.cl>
2023-11-16 17:03:09 -0300

    Cambio en hora

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -25,8 +25,40 @@ app.get('/servicios', function (req, res) {
     res.render('servicios');
   });
 
+app.get('/ver_horas',function(req,res){
+  var disponible = [];
+  var fechas = []; 
+  const query = 'CALL PRC_VER_HORA()';
+  mysqlConnection.query(query,(error,results)=>{
+    if (error) {
+      console.error('Error executing query:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    results[0].forEach(element => {
+      
+      if(element.ESTADO == 0){
+        disponible.push(element);
+        fechas.push(element.FECHA_INI);
+      }
+    });
+    
+    const uniqueDatesSet = new Set(fechas);
+    // Convert Set back to array
+    const uniqueDatesArray = Array.from(uniqueDatesSet);
+    const sorteado = uniqueDatesArray.sort((a, b) => {
+      // Assuming date format is DD/MM/YYYY
+      const dateA = new Date(a.split("/").reverse().join("/"));
+      const dateB = new Date(b.split("/").reverse().join("/"));
+      return dateA - dateB;
+    });
+    res.render('hora',{ dates : sorteado, horas : disponible })
+  })  
+});  
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.post('/validar', function (req, res) {
     const datos = req.body;
     let descripcion = datos.descripcion;
@@ -41,8 +73,8 @@ app.post('/validar', function (req, res) {
     });
   
     console.log(datos);
-  });
+});
 
-  app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
 });
